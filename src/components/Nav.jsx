993f211e53e911fdc0e5431e83db78c5195ad693
Thuta-21 <thutakyaw9125@gmail.com>
2025-11-07 13:13:13 +0630

    fixed responsive

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,23 @@
 import { headerLogo } from "../assets/images";
 import { hamburger } from "../assets/icons";
 import { navLinks } from "../constants/constants.index.js";
-
+import { useState } from "react";
 const Nav = () => {
+  const [hide, setHide] = useState('hidden');
+  const handleNav = () => {
+    setHide(hide === 'hidden' ? 'flex' : 'hidden');
+  }
   return (
-    <header className="padding-x py-8 absolute z-10 w-full">
+    <header className="padding-x py-8 sticky top-0 z-20 w-full bg-white-400 ">
       <nav className="flex justify-between items-center max-container">
         <a href="/">
-          <img src={headerLogo} alt="Logo" width={130} height={29} className='m-0 w-[129px] h-[29px]'/>
+          <img
+            src={headerLogo}
+            alt="Logo"
+            width={130}
+            height={29}
+            className="m-0 w-[129px] h-[29px]"
+          />
         </a>
         <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
           {navLinks.map((item) => (
@@ -21,10 +31,24 @@ const Nav = () => {
             </li>
           ))}
         </ul>
-        <div className="hidden max-lg:block">
+        <div className="hidden max-lg:block" onClick={handleNav}>
           <img src={hamburger} alt="Hamburger" width={25} height={25} />
         </div>
       </nav>
+      <div className={`max-lg:${hide} justify-center lg:hidden`}>
+        <ul className="flex-1 flex flex-col absolute justify-center items-center gap-16 mt-20 ">
+          {navLinks.map((item) => (
+            <li key={item.label + '2'} >
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray max-lg:text-white bg-gray-400 p-3 rounded-full shadow-lg"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
     </header>
   );
 };
